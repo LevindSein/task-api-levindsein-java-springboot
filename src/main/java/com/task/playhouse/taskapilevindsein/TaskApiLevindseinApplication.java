7a63bff51603912ed673a369d3adad77833a7583
Fahni Amsyari <fahnialfatih@gmail.com>
2021-08-02 20:40:58 +0700

    first commit

---
+++ src/main/java/com/task/playhouse/taskapilevindsein/TaskApiLevindseinApplication.java
@@ -0,0 +1,13 @@
+package com.task.playhouse.taskapilevindsein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApiLevindseinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApiLevindseinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/playhouse/taskapilevindsein/TaskApiLevindseinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.playhouse.taskapilevindsein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiLevindseinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
